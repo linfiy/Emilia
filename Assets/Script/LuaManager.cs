31d5cae8c68c9c81e879c9d3d8c2fc4c9bbc4b51
wangliang <574096324@qq.com>
2017-09-01 14:34:14 +0800

    梳理

--- Assets/Script/LuaManager.cs
+++ Assets/Script/LuaManager.cs
@@ -1,14 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 using XLua;
-public class LuaManager : MonoBehaviour {
+using System.IO;
+using System.Collections.Generic;
+[Hotfix]
+public class LuaManager : MonoBehaviour
+{
+  public int time = 1;
+  Dictionary<string, AssetBundle> bundles = new Dictionary<string, AssetBundle>();
+  LuaEnv env;
+  void Awake()
+  {
+    env = new LuaEnv();
+    env.AddLoader(new LuaEnv.CustomLoader((ref string file) =>
+    {
+      file = Application.streamingAssetsPath + "/Lua/Resources/" + file + ".lua";
+      string text = File.ReadAllText(file);
+      if (text != null) return System.Text.Encoding.UTF8.GetBytes(text);
+      return null;
+    }));
+  }
+  void Start()
+  {
 
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  }
+  void Update()
+  {
+    // print("C#"+(time++));
+    env.Tick();
+  }
+
+  void OnGUI()
+  {
+    if (GUI.Button(new Rect(0, 0, 400, 200), "热更新"))
+    {
+      env.DoString("require 'ShowTime'");
+    }
+  }
 }
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 xlua, unirx
-
-
+#说明
+1.Lua代码在Assets/Lua/Resources/下编写    
+2.Lualoader的路径为Assets/StreamingAssets/Lua/Resources/    
+3.Lua代码不用加后缀.txt,直接用.lua即可.     
+4.每次写完lua代码需要进行Build拷贝,点击工具栏的AssetsBundle/BuildAssets即可,然后点XLua/GenerateCode,运行.
 
 # 当前使用方法
 
